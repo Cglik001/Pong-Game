459feec3025c9248fc84a9607a6bb0fcbf0f2b7d
christian landsberger Cglik001@gmail.com <cglik001@gmail.com>
2022-05-04 17:51:57 -0300

    trying to fix diagonal collision

--- assets/scripts/pong-multiplayer/main4.js
+++ assets/scripts/pong-multiplayer/main4.js
@@ -46,12 +46,20 @@ class Element {
 }
 // draw net
 const net = {
-  x: canvas.width/2,
+  x: 335,
   y: 0,
   width: 4,
   height: canvas.height,
-  color: "#ffff",
+  color: "#ffff"
 }
+
+/*const net = {
+  x: canvas.width/2 - 1,
+  y: 0,
+  width: 2,
+  height: 400,
+  color: "WHITE"
+}*/
 //first paddle
 const playerOne = new Element({
   x:10,
@@ -73,6 +81,16 @@ const playerTwo = new Element({
   gravity: 2,
 })
 
+//cpu
+const cpu = new Element( {
+  x:625,
+  y:155,
+  width: 15,
+  height: 90,
+  color: "#fff",
+  gravity: 2,
+})
+
 //ball
 
 const ball = new Element({
@@ -81,11 +99,19 @@ const ball = new Element({
   width: 15,
   height: 15,
   color: "#20C20E",
-  speed: 1,
+  speed: 2,
   gravity: 2
 })
 
 
+/*function drawNet (){
+  ctx.beginPath();
+  ctx.setLineDash([5, 15]);
+  ctx.moveTo(0, 50);
+  ctx.lineTo(300, 50);
+  ctx.stroke();
+
+}*/
 //player one score text
 
 function displayScoreOne(){
@@ -102,13 +128,30 @@ function displayScoreTwo(){
 }
 
 
+/*function drawRect(x,y,w,h,color){
+  context.fillStyle = color;
+  context.fillRect(x,y,w,h);
+}
 
+function drawNet(){
+  for(let i = 0; i <= canvas.height; i+=15){
+    drawRect(net.x, net.y + i, net.width, net.height, net.color)
+    drawElement(net.x, net.y + i, net.width, net.height, net.color)
+  }*/
+
+ /* function drawNet(){
+     for(let i = 0; i <= canvas.height; i+=15){
+       drawElement(net.x, net.y + i, net.width, net.height, net.color)
+     }
+  }*/
 
 //draw elements
 function drawElement(element){
   context.fillStyle = element.color;
   context.fillRect(element.x, element.y, element.width, element.height)
 }
+
+
 ////drawElement(playerOne);
 
 //make ball bounce
@@ -129,7 +172,91 @@ function ballBounce(){
 
 function paddleCollision(){
   //fazer a bolinha voltar quando bater no paddle
-  if (
+   /*if (
+     (
+      (
+        ball.y + ball.width + ball.height > playerOne.y
+        && ball.y - ball.width <= playerOne.y + playerOne.height
+        )
+      && (ball.x - ball.width === playerOne.x + playerOne.width && ball.speed < 0)) ||
+      (
+        (ball.y + ball.width > playerTwo.y &&
+         ball.y - ball.width <= playerTwo.y + playerTwo.height)
+          && (ball.x + ball.width === playerTwo.x && ball.speed > 0))) {
+    console.log("hit")
+    ball.speed = ball.speed * -1;
+  }*/
+
+   /* if(
+      (
+        (
+          ball.y <= playerTwo.y + playerTwo.height
+          && ball.y > playerTwo.y
+          )
+        && (ball.x + ball.width === playerTwo.x && ball.speed > 0)
+      )
+      ||
+      (
+        (
+          ball.y <= playerOne.y + playerOne.height
+          && ball.y > playerOne.y
+          )
+        && (ball.x === playerOne.x + playerOne.width && ball.speed < 0)
+      )
+    ) {
+      ball.speed = ball.speed * -1;
+    } */
+ if (
+     (
+      (
+        ball.y + (ball.width + ball.height) > playerOne.y
+        && ball.y - (ball.width + ball.height) <= playerOne.y + playerOne.height
+        )
+      && (ball.x  === playerOne.x + playerOne.width && ball.speed < 0)) ||
+      (ball.y + ball.width + ball.height > playerTwo.y
+        && ball.y + ball.height <= playerTwo.y + playerTwo.height + playerTwo.width
+        )
+      && (ball.x + ball.height >= playerTwo.x && ball.speed > 0)) {
+      console.log("hit")
+     ball.speed = ball.speed * -1;
+  }
+
+
+
+    /*  kind of works   if (
+     (
+      (
+        ball.y + (ball.width + ball.height) > playerOne.y
+        && ball.y - (ball.width + ball.height) <= playerOne.y + playerOne.height
+        )
+      && (ball.x  === playerOne.x + playerOne.width && ball.speed < 0)) ||
+      (
+        (ball.y + ball.width + ball.height <= playerTwo.y + playerTwo.height + playerTwo.width &&
+         ball.y + ball.width + ball.height <= playerTwo.y + playerTwo.height)
+          && (ball.x + ball.width === playerTwo.x && ball.speed > 0))) {
+    console.log("hit")
+    ball.speed = ball.speed * -1;
+  }*/
+
+
+  /*  if (
+     (
+      (
+        ball.y + ball.height + ball.gravity > playerOne.y &&  ball.y + ball.width + ball.gravity > playerOne.y
+        &&  ball.y + ball.height + ball.gravity  <= playerOne.height
+        )
+      && (ball.x === playerOne.x + playerOne.width && ball.speed < 0)) ||
+      (
+        (ball.y + ball.height + ball.gravity  > playerTwo.y && ball.y + ball.width + ball.gravity  > playerOne.y
+        && ball.y + ball.height + ball.gravity <= playerTwo.height)
+          && (ball.x + ball.width === playerTwo.x && ball.speed > 0))) {
+    console.log("hit")
+    ball.speed = ball.speed * -1;
+  }*/
+
+
+
+  /*if (
      (
       (
         ball.y > playerOne.y
@@ -142,7 +269,47 @@ function paddleCollision(){
           && (ball.x + ball.width === playerTwo.x && ball.speed > 0))) {
     console.log("hit")
     ball.speed = ball.speed * -1;
-  }
+  }*/
+
+  //ball.x + b.width >  player.x &&  ball.y + ball.width > player.y && ball.x - ball.radius < ball.x + ball.radius
+  //&& ball.y - ball.radius < player.y + player.height
+
+    //return b.right > p.left && b.bottom > p.top && b.left < b.right &&
+  //b.top < p.bottom;
+
+  //b.x + b.radius >  p.x &&  b.y + b.radius > p.y && b.x - b.radius < b.x + b.radius
+  //&& b.y - b.radius < p.y + p.height
+
+   /* if (
+     (
+      (
+        (b.bottom ) ball.y > player.top
+        && b.top  <= p.bottom
+        )
+      && (b.left  ===  p.right  && ball.speed < 0)) ||
+      (
+        (b.bottom  >  p.top &&
+         b.top  <= p.bottom)
+          && ( b.right === p.left  && ball.speed > 0))) {
+    console.log("hit")
+    ball.speed = ball.speed * -1;
+  }*/
+
+/*function collision(b,p){
+  b.top = b.y - b.radius;
+  b.bottom = b.y + b.radius;
+  b.left = b.x - b.radius;
+  b.right = b.x + b.radius;
+
+  p.top = p.y;
+  p.bottom = p.y + p.height;
+  p.left = p.x;
+  p.right = p.x + p.width;
+
+  return b.right > p.left && b.bottom > p.top && b.left < b.right &&
+  b.top < p.bottom;
+}*/
+
 
 
   //
@@ -185,16 +352,6 @@ function resetGame() {
   playerOne.y = 155;
   playerTwo.y = 155;
   resetBallPosition() ;
-}
-
-function enableButtons() {
-  hardBtn.removeAttribute('disabled')
-  normalBtn.removeAttribute('disabled')
-}
-
-function disableButtons() {
-    hardBtn.setAttribute('disabled', true)
-    normalBtn.setAttribute('disabled', true)
 }
 
 
@@ -246,23 +403,41 @@ function drawElements(){
   context.clearRect(0,0, canvas.width, canvas.height)
   drawElement(playerOne);
   drawElement(playerTwo);
+  drawElement(net);
   drawElement(ball);
   displayScoreOne();
   displayScoreTwo();
-  drawElement(net);
+  gameWinner()
+
 }
 
 function hardGame(){
-  ball.speed = 4;
+  paddleCollision();
+  ballBounce();
+  ballwallPoints();
+  ball.speed += 6 * -1;
   playerOne.gravity = 6;
   playerTwo.gravity = 6;
 }
 
 function normalGame() {
-  ball.speed = 1;
-  playerOne.gravity = 2;
-  playerTwo.gravity = 2;
+  ballBounce();
+  drawElements();
+  paddleCollision();
+  ballwallPoints();
 }
+
+function enableButtons() {
+  hardBtn.removeAttribute('disabled')
+  normalBtn.removeAttribute('disabled')
+}
+
+function disableButtons() {
+    hardBtn.setAttribute('disabled', true)
+    normalBtn.setAttribute('disabled', true)
+}
+
+
 
 /*function gameWinner() {
   if(scoreOne === 10) {
@@ -276,6 +451,38 @@ function normalGame() {
     }
 
 }*/
+function gameWinner() {
+  if(scoreOne === 3) {
+      alert('player One Wins!')
+  }
+  else if (scoreTwo === 3) {
+       alert('Player Two Wins!')
+            }
+  //gameOver()
+}
+
+/*function startGame(){
+  let startDiv = document.getElementById("start")
+  let gameCanvas = document.getElementById("pongGame")
+  let gameOver = document.getElementById("game-over");
+  startDiv.style.display = "none";
+  gameCanvas.style.display = "block";
+  gameOver.style.display = "none";
+  start();
+}
+
+function gameOver() {
+  let startDiv = document.getElementById("start")
+  let gameCanvas = document.getElementById("pongGame")
+  let gameOver = document.getElementById("game-over");
+  startDiv.style.display = "none";
+  gameCanvas.style.display = "none";
+  gameOver.style.display = "block";
+  resetGame() ;
+  clearInterval(loop)
+
+}
+*/
 
 
 
@@ -313,3 +520,6 @@ const net = {
     ball.y += ball.gravity;
     ball.x += ball.speed;
   }*/
+
+ hardBtn.addEventListener('click', hardGame)
+ normalBtn.addEventListener('click', normalGame)
